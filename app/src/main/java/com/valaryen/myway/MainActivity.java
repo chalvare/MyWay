d4970f685e8221621a1f15345d2722f19ca9dbca
Altair <chalvare@ucm.es>
2016-02-24 21:04:10 +0100

    Comentarios

--- app/src/main/java/com/valaryen/myway/MainActivity.java
+++ app/src/main/java/com/valaryen/myway/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //asasas
         setSupportActionBar(toolbar);
 
 
